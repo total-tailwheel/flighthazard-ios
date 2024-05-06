9432efc47f3f167edde29f112ca612a1c9373028
Kevin Gilpin <kgilpin@gmail.com>
2024-05-06 17:06:52 -0400

    feat: Fetch airtports and flight plans

--- Flight Hazard/ContentView.swift
+++ Flight Hazard/ContentView.swift
@@ -1,21 +1,102 @@
+// File path: Flight Hazard/Flight Hazard/ContentView.swift
+
 //
 //  ContentView.swift
 //  Flight Hazard
 //
 //  Created by Kevin Gilpin on 5/2/24.
+//  Modified by Navie on 5/3/24 to include airport and flight plan lists.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @Binding var airports: [Airport]
+  @Binding var flightPlans: [FlightPlan]
+
+  var body: some View {
+    ScrollView {
+      VStack {
+        Text("Airports")
+          .font(.headline)
+          .padding()
+
+        List(airports, id: \.icao_code) { airport in
+          AirportView(airport: airport)
+        }
+        .frame(height: 200)
+        .onAppear {
+          FlightDataService.getAirports { (fetchedAirports, error) in
+            if let airports = fetchedAirports {
+              self.airports = airports
+            } else if let error = error {
+              print("Error fetching airports: \(error.localizedDescription)")
+            }
+          }
+        }
+
+        Text("Flight Plans")
+          .font(.headline)
+          .padding()
+
+        List(flightPlans, id: \.departure_airport) { flightPlan in
+          FlightPlanView(flightPlan: flightPlan)
+        }
+        .frame(height: 200)
+        .onAppear {
+          FlightDataService.getFlightPlans { (fetchedPlans, error) in
+            if let flightPlans = fetchedPlans {
+              self.flightPlans = flightPlans
+            } else if let error = error {
+              print("Error fetching flight plans: \(error.localizedDescription)")
+            }
+          }
+        }
+      }
     }
+  }
+}
+
+struct AirportView: View {
+  var airport: Airport
+
+  var body: some View {
+    Text(airport.icao_code)
+  }
+}
+
+struct FlightPlanView: View {
+  var flightPlan: FlightPlan
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(
+        "From: \(flightPlan.departure_airport.icao_code) To: \(flightPlan.destination_airport.icao_code)"
+      )
+      .font(.subheadline)
+      Text("Date: \(flightPlan.departure_time, style: .date)")
+        .font(.caption)
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    // Mock data for preview purposes
+    let mockAirports = [Airport(icao_code: "XYZ"), Airport(icao_code: "ABC")]
+    let mockFlightPlans = [
+      FlightPlan(
+        departure_airport: mockAirports[0], destination_airport: mockAirports[1],
+        departure_time: Date()
+      )
+    ]
+
+    // Creating bindings from state
+    let airportsBinding = Binding.constant(mockAirports)
+    let flightPlansBinding = Binding.constant(mockFlightPlans)
+
+    // Or: .constant([Airport(icao_code: "SampleICAO")])
+    // Or: .constant([FlightPlan(departure_airport: Airport(icao_code: "SampleICAO"), destination_airport: Airport(icao_code: "SampleICAO"), departure_time: Date())])
+    ContentView(airports: airportsBinding, flightPlans: flightPlansBinding)
+  }
 }
---
+++ Flight Hazard/FlightDataService.swift
@@ -0,0 +1,77 @@
+// File path: Flight Hazard/Flight Hazard/FlightDataService.swift
+
+import Foundation
+
+struct Airport: Decodable, Hashable {
+  var icao_code: String
+}
+
+struct FlightPlan: Decodable, Hashable {
+  var departure_airport: Airport
+  var destination_airport: Airport
+  var departure_time: Date
+
+  enum CodingKeys: String, CodingKey {
+    case departure_airport, destination_airport, departure_time
+  }
+
+  init(departure_airport: Airport, destination_airport: Airport, departure_time: Date) {
+    self.departure_airport = departure_airport
+    self.destination_airport = destination_airport
+    self.departure_time = departure_time
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    departure_airport = try container.decode(Airport.self, forKey: .departure_airport)
+    destination_airport = try container.decode(Airport.self, forKey: .destination_airport)
+
+    let dateFormatter = ISO8601DateFormatter()
+      dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+
+    let dateStr = try container.decode(String.self, forKey: .departure_time)
+    guard let date = dateFormatter.date(from: dateStr) else {
+      throw DecodingError.dataCorruptedError(
+        forKey: .departure_time,
+        in: container,
+        debugDescription: "Date string does not match format expected by formatter.")
+    }
+    departure_time = date
+  }
+}
+
+// Data service for fetching airports and flight plans.
+struct FlightDataService {
+  // Generic data fetching function
+  private static func fetchData<T: Decodable>(
+    from url: URL, completion: @escaping (T?, Error?) -> Void
+  ) {
+    var request = URLRequest(url: url)
+    request.setValue("no-cache, no-store, must-revalidate", forHTTPHeaderField: "Cache-Control")
+    request.setValue("no-cache", forHTTPHeaderField: "Pragma")
+    request.setValue("0", forHTTPHeaderField: "Expires")
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+      if let error = error {
+        completion(nil, error)
+        return
+      }
+      guard let data = data, let decoded = try? JSONDecoder().decode(T.self, from: data) else {
+        completion(nil, NSError(domain: "", code: -1, userInfo: nil))
+        return
+      }
+      completion(decoded, nil)
+    }
+    task.resume()
+  }
+
+  static func getAirports(completion: @escaping ([Airport]?, Error?) -> Void) {
+    let url = URL(string: "http://localhost:4000/api/airports")!
+    fetchData(from: url, completion: completion)
+  }
+
+  static func getFlightPlans(completion: @escaping ([FlightPlan]?, Error?) -> Void) {
+    let url = URL(string: "http://localhost:4000/api/flight-plans")!
+    fetchData(from: url, completion: completion)
+  }
+}
